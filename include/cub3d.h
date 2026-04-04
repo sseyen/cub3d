d41cb4faad880b1fa1a802fcfb133d74e39a33cc
Alexandr Lisseyenko <krisstalexx@gmail.com>
2026-04-04 12:12:46 +0200

    style: add header

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/04 12:08:03 by alisseye          #+#    #+#             */
+/*   Updated: 2026/04/04 12:11:38 by alisseye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #ifndef CUB3D_H
 # define CUB3D_H
@@ -9,12 +21,12 @@
 # define ROT_SPEED 0.03
 
 # include <fcntl.h>		// open, close
-# include <math.h>
-# include <unistd.h>		// read, write
+# include <math.h>		// floor, fabs
+# include <unistd.h>	// read, write
 # include <stdio.h>		// printf, perror, strerror
-# include <stdlib.h>		// malloc, free, exit
+# include <stdlib.h>	// malloc, free, exit
 # include <sys/time.h>	// gettimeofday
-# include <stdbool.h>
+# include <stdbool.h>	// bool type
 
 # include <MLX42/MLX42.h>
 
@@ -90,71 +102,71 @@ typedef struct s_ray
 }	t_ray;
 
 /* cleanup_game.c */
-void	cleanup_textures(t_textures *textures);
-void	cleanup_map(t_map *map);
-void	cleanup_game(t_game *game);
+void		cleanup_textures(t_textures *textures);
+void		cleanup_map(t_map *map);
+void		cleanup_game(t_game *game);
 
 /* close_game.c */
-void	close_game(void *param);
+void		close_game(void *param);
 
 /* errors.c */
-int		error_msg(char *str);
+int			error_msg(char *str);
 
 /* game_loop.c */
-void	game_loop(void *param);
+void		game_loop(void *param);
 
 /* handle_movement.c */
-void	move_forward_back(t_player *p, t_map *map, int forward);
-void	move_side(t_player *p, t_map *map, int right);
-void	rotate_player(t_player *p, int right);
-void	handle_movement(t_game *game);
+void		move_forward_back(t_player *p, t_map *map, int forward);
+void		move_side(t_player *p, t_map *map, int right);
+void		rotate_player(t_player *p, int right);
+void		handle_movement(t_game *game);
 
 /* init_game.c */
-int		init_game(t_game *game);
-int		set_direction(t_player *player, char spawn);
-int		init_textures(t_game *game);
-int		init_player(t_game *game);
+int			init_game(t_game *game);
+int			set_direction(t_player *player, char spawn);
+int			init_textures(t_game *game);
+int			init_player(t_game *game);
 
 /* init_structs.c */
-int		init_structs(t_game *game);
+int			init_structs(t_game *game);
 
 /* key_press.c */
-void	key_press(mlx_key_data_t keydata, void *param);
+void		key_press(mlx_key_data_t keydata, void *param);
 
 /* parse_file.c */
-int		only_map_chars(char *line);
-int		correct_extension(char *path, char *ext, int ext_len);
-int		parse_header(int fd, t_game *game);
-int		parse_map(int fd, t_game *game);
-int		parse_file(char *path, t_game *game);
+int			only_map_chars(char *line);
+int			correct_extension(char *path, char *ext, int ext_len);
+int			parse_header(int fd, t_game *game);
+int			parse_map(int fd, t_game *game);
+int			parse_file(char *path, t_game *game);
 
 /* parse_header.c */
-int		parse_texture(char *line, char **texture_path, int i);
-int		valid_color_input(char *line, int **color, int i, int j);
-int		parse_color(char *line, int **color, int i);
-int		validate_header(t_game *game);
-int		parse_header_line(char *line, t_game *game);
+int			parse_texture(char *line, char **texture_path, int i);
+int			valid_color_input(char *line, int **color, int i, int j);
+int			parse_color(char *line, int **color, int i);
+int			validate_header(t_game *game);
+int			parse_header_line(char *line, t_game *game);
 
 /* parse_map.c */
-int		append_line(t_map *map, char *line);
-int		collect_map_lines(int fd, t_game *game);
-int		validate_map_chars(t_game *game);
-int		is_open(t_map *map, int y, int x);
-int		validate_map_closed(t_game *game);
+int			append_line(t_map *map, char *line);
+int			collect_map_lines(int fd, t_game *game);
+int			validate_map_chars(t_game *game);
+int			is_open(t_map *map, int y, int x);
+int			validate_map_closed(t_game *game);
 
 /* raycasting.c */
-void	raycasting_math(t_game *game);
-void	init_ray(t_ray *ray, t_player *p, int x);
-void	init_dda(t_ray *ray, t_player *p);
-void	perform_dda(t_ray *ray, t_map *map);
-void	calc_wall_height(t_ray *ray);
+void		raycasting_math(t_game *game);
+void		get_ray_direction(t_ray *ray, t_player *p, int x);
+void		calc_step_distance(t_ray *ray, t_player *p);
+void		find_nearest_wall(t_ray *ray, t_map *map);
+void		calc_wall_height(t_ray *ray);
 
 /* render.c */
-void	draw_column(t_game *game, t_ray *ray, int x);
-void	draw_ceiling(t_game *game, t_ray *ray, int x);
-void	draw_floor(t_game *game, t_ray *ray, int x);
-void	draw_wall(t_game *game, t_ray *ray, int x);
-int		get_tex_x(t_ray *ray, t_player *p);
+void		draw_column(t_game *game, t_ray *ray, int x);
+void		draw_ceiling(t_game *game, t_ray *ray, int x);
+void		draw_floor(t_game *game, t_ray *ray, int x);
+void		draw_wall(t_game *game, t_ray *ray, int x);
+int			get_tex_offset(t_ray *ray, t_player *p);
 
 /* texture.c */
 t_texptr	get_wall_texture(t_game *game, t_ray *ray);
@@ -162,6 +174,6 @@ uint32_t	get_tex_color(mlx_texture_t *tex, int tex_x, int tex_y);
 uint32_t	rgb_to_color(int *rgb);
 
 /* utils.c */
-char	*mini_gnl(int fd);
+char		*mini_gnl(int fd);
 
 #endif
--- include/input.h
+++ include/input.h
@@ -1,27 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/04 12:08:06 by alisseye          #+#    #+#             */
+/*   Updated: 2026/04/04 12:08:55 by alisseye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #ifndef INPUT_H
 # define INPUT_H
 
-// map chars
-#define WALL '1'
-#define FLOOR '0'
-#define SPACE ' '
-#define PLAYER_NORTH 'N'
-#define PLAYER_SOUTH 'S'
-#define PLAYER_EAST 'E'
-#define PLAYER_WEST 'W'
+/* map chars */
+# define WALL '1'
+# define FLOOR '0'
+# define SPACE ' '
+# define PLAYER_NORTH 'N'
+# define PLAYER_SOUTH 'S'
+# define PLAYER_EAST 'E'
+# define PLAYER_WEST 'W'
 
-#define TILE_SIZE 64
+/* raycasting */
+# define TILE_SIZE 64
 
-// MLX42 key aliases
-#define KEY_ESC   MLX_KEY_ESCAPE
-#define KEY_W     MLX_KEY_W
-#define KEY_A     MLX_KEY_A
-#define KEY_S     MLX_KEY_S
-#define KEY_D     MLX_KEY_D
-#define KEY_LEFT  MLX_KEY_LEFT
-#define KEY_RIGHT MLX_KEY_RIGHT
-#define KEY_UP    MLX_KEY_UP
-#define KEY_DOWN  MLX_KEY_DOWN
+/* MLX42 key aliases */
+# define KEY_ESC   MLX_KEY_ESCAPE
+# define KEY_W     MLX_KEY_W
+# define KEY_A     MLX_KEY_A
+# define KEY_S     MLX_KEY_S
+# define KEY_D     MLX_KEY_D
+# define KEY_LEFT  MLX_KEY_LEFT
+# define KEY_RIGHT MLX_KEY_RIGHT
+# define KEY_UP    MLX_KEY_UP
+# define KEY_DOWN  MLX_KEY_DOWN
 
 #endif
